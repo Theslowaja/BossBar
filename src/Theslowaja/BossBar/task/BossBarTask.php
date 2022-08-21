e28916e86bd9b71f5f28faac6d170c218fe7b3ff
Theslowaja <67841659+Theslowaja@users.noreply.github.com>
2022-08-21 07:48:13 +0700

    Testing Task #6

--- src/Theslowaja/BossBar/task/BossBarTask.php
+++ src/Theslowaja/BossBar/task/BossBarTask.php
@@ -7,8 +7,9 @@ use pocketmine\scheduler\Task;
 class BossBarTask extends Task{
 
     private Loader $plugin;
+    private $count;
 
-    public __construct(Loader $pl, Player $p){
+    public function __construct(Loader $pl, Player $p){
         $this->pl = $pl;
         $this->player = $p;
         $this->count = 0;
@@ -17,7 +18,7 @@ class BossBarTask extends Task{
     public function onRun() : void {
         	$persen = $this->getConfig()->get("percentage");
         	$top = str_replace("&", "§", $this->plugin->getConfig()->getAll()["BossBar"]["Top-Title"]);
-                $top = str_replace("&", "§", $this->plugin->getConfig()->getAll()["BossBar"]["Sub-Title"]);
+                $sub = str_replace("&", "§", $this->plugin->getConfig()->getAll()["BossBar"]["Sub-Title"]);
     		back:
     		if($this->count < count($messages)){
     	    		$this->pl->sendBossBar($this->player, $top[$this->count], $sub[$this->count], $persen);
