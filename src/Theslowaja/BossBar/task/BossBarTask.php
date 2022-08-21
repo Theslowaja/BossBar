0b8476bdbedabd5bdf599fe977a50c5653c2e8c1
Theslowaja <67841659+Theslowaja@users.noreply.github.com>
2022-08-22 04:59:00 +0700

    Testing Task #11

--- src/Theslowaja/BossBar/task/BossBarTask.php
+++ src/Theslowaja/BossBar/task/BossBarTask.php
@@ -3,26 +3,26 @@
 namespace Theslowaja\BossBar\task;
 
 use pocketmine\scheduler\Task;
-use pocketmine\player\Player;
 use Theslowaja\BossBar\Loader;
 
 class BossBarTask extends Task{
 
     private Loader $pl;
+
     private $count;
 
-    public function __construct(Loader $pl){
-        $this->pl = $pl;
+    public function __construct(Loader $plugin){
+        $this->plugin = $plugin;
         $this->count = 0;
     }
 
     public function onRun() : void {
-        	$persen = $this->pl->getConfig()->get("percentage");
-        	$top = str_replace("&", "§", $this->pl->getConfig()->getAll()["BossBar"]["Top-Title"]);
-                $sub = str_replace("&", "§", $this->pl->getConfig()->getAll()["BossBar"]["Sub-Title"]);
+        	$persen = $this->plugin->getConfig()->get("percentage");
+        	$top = str_replace("&", "§", $this->plugin->getConfig()->getAll()["BossBar"]["Top-Title"]);
+                $sub = str_replace("&", "§", $this->plugin->getConfig()->getAll()["BossBar"]["Sub-Title"]);
     		back:
     		if($this->count < count($top)){
-    	    		$this->pl->sendBossBar($top[$this->count], $sub[$this->count], $persen);
+    	    		$this->plugin->sendBossBar($top[$this->count], $sub[$this->count], $persen);
     	    		$this->count++;
     		}else{
 		    	$this->count = 0;
