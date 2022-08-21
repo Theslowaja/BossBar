cdaffa58de06dbc89dd8ea252f785fa9b9c5bbb1
Theslowaja <67841659+Theslowaja@users.noreply.github.com>
2022-08-21 07:58:34 +0700

    Testing Task #9

--- src/Theslowaja/BossBar/task/BossBarTask.php
+++ src/Theslowaja/BossBar/task/BossBarTask.php
@@ -9,6 +9,7 @@ use Theslowaja\BossBar\Loader;
 class BossBarTask extends Task{
 
     private Loader $pl;
+    private Player $player;
     private $count;
 
     public function __construct(Loader $pl, Player $p){
@@ -18,11 +19,11 @@ class BossBarTask extends Task{
     }
 
     public function onRun() : void {
-        	$persen = $this->getConfig()->get("percentage");
-        	$top = str_replace("&", "§", $this->plugin->getConfig()->getAll()["BossBar"]["Top-Title"]);
-                $sub = str_replace("&", "§", $this->plugin->getConfig()->getAll()["BossBar"]["Sub-Title"]);
+        	$persen = $this->pl->getConfig()->get("percentage");
+        	$top = str_replace("&", "§", $this->pl->getConfig()->getAll()["BossBar"]["Top-Title"]);
+                $sub = str_replace("&", "§", $this->pl->getConfig()->getAll()["BossBar"]["Sub-Title"]);
     		back:
-    		if($this->count < count($messages)){
+    		if($this->count < count($top)){
     	    		$this->pl->sendBossBar($this->player, $top[$this->count], $sub[$this->count], $persen);
     	    		$this->count++;
     		}else{
